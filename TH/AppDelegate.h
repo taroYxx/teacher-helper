c984667063d30a1691abb060e03ee3248e767ef9
taroYxx <446734081@qq.com>
2015-11-22 12:43:34 +0800

    Initial Commit

---
+++ TH/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TH
+//
+//  Created by Taro on 15/11/22.
+//  Copyright © 2015年 Taro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TH/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TH
+//
+//  Created by Taro on 15/11/22.
+//  Copyright © 2015年 Taro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
